aebfbc29c83dc8422bdaa1e55001d095418dad3c
Erica Sue <erica.sue143@gmail.com>
2022-04-25 17:50:47 -0400

    Update files

---
+++ main.js
@@ -0,0 +1,55 @@
+class Stopwatch {
+     constructor(id, delay=100) {
+       this.state = "paused";
+       this.delay = delay;
+       this.timer = document.getElementById('timer');
+       this.value = 0;
+     }
+     
+     time(ms) {
+       let hours   = Math.floor(ms / 3600000);
+       let minutes = Math.floor((ms - (hours * 3600000)) / 60000);
+       let seconds = Math.floor((ms - (hours * 3600000) - (minutes * 60000)) / 1000);
+       let ds = Math.floor((ms - (hours * 3600000) - (minutes * 60000) - (seconds * 1000))/100);
+    
+       if (hours   < 10) {hours   = "0"+hours;}
+       if (minutes < 10) {minutes = "0"+minutes;}
+       if (seconds < 10) {seconds = "0"+seconds;}
+       return hours+':'+minutes+':'+seconds+'.'+ds;
+     }
+     
+     update() {
+       if (this.state=="running") {
+         this.value += this.delay;
+       }
+       this.timer.innerHTML = this.time(this.value);
+     }
+     
+     start() {
+       if (this.state=="paused") {
+         this.state="running";
+         if (!this.interval) {
+           var t=this;
+           this.interval = setInterval(function(){t.update();}, this.delay);
+         }
+       }
+     }
+     
+     stop() {
+          if (this.state=="running") {
+         this.state="paused";
+       if (this.interval) {
+         clearInterval(this.interval);
+         this.interval = null;
+       }
+          }
+     }
+     
+     reset() {
+       this.stop();
+       this.value=0;
+       this.update();
+     }
+   }
+
+   stopwatch = new Stopwatch("stopwatch");
